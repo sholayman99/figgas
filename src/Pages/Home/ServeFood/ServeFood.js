c2f2fc0eb3e1cad212296e8dc979f4a8e09cf4d3
Md Sholayman <mdsholayman998@gmail.com>
2022-12-06 13:18:09 +0600

    recorrection in ui

--- src/Pages/Home/ServeFood/ServeFood.js
+++ src/Pages/Home/ServeFood/ServeFood.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import TabsUi from '../TabsUi/TabsUi';
+import './ServeFood.css'
 
 const ServeFood = () => {
     return (
         <div>
-            
+         <div className='serveFood-text'>
+         <h2 className=''>What kind of Foods we serve for you</h2> 
+          <p>Who are in extremely love with eco friendly system.</p> 
+         </div>
+         <TabsUi />
         </div>
     );
 };
